e72abeaace4506b4ad5af980e9e6ba61a0a279e5
Elina WebMagic <98823823+elina-web-magic@users.noreply.github.com>
2026-06-15 20:43:40 +0200

    Reorder imports to match Biome sorting rules

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,7 @@
-import { Pool } from 'pg'
-
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
+
+import { Pool } from 'pg'
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined }
 
