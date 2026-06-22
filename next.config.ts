71a3f527dfb966facae5bd66b7422c8edeef8635
Elina Dzhelilova <98823823+elinadzhelilova-web@users.noreply.github.com>
2026-06-22 17:38:38 +0200

    fix: vercel build, installed client runtime utils from prisma

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-	/* config options here */
+	serverExternalPackages: [
+		'@prisma/client',
+		'prisma',
+		'@prisma/client-runtime-utils',
+		'@/generated/prisma',
+	],
 };
 
 export default nextConfig;
